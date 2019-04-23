036c472950ba5b2bb885c6daec3af634b9a58c8b
lucotmo <lucotmo@gmail.com>
2019-04-23 17:19:08 -0500

    0.1.0

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+     <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Vue.JS SPA example</div>
+
+                    <div class="card-body">
+                        <h2>{{ welcome }}</h2>
+                        <img src="https://i.vimeocdn.com/portrait/27617066_300x300" alt="Lucotmo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        computed: {
+            welcome(){
+                return this.$store.getters.welcome
+            }
+        }
+    }
+</script>
